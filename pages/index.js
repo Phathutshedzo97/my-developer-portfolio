76a1beab0d19c4d059c2168ca4a61e1caff198f2
Phathutshedzo97 <156464392+Phathutshedzo97@users.noreply.github.com>
2024-11-27 13:43:24 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <Layout>
       <div>
-      <h1>Hi and Welcome to my devloper protfolio</h1>
+      <h1>Hi and Welcome to my devloper portfolio</h1>
       <p>Hello and welcome to my portfolio I'm Phathutshedzo Mudau, a passionate accountant turned aspiring full-stack developer. This site is where I share my adventures, in the world of technology.</p>
       <p>Thank you for visiting my site, and I hope you enjoy your time here. Let's connect, learn, and explore together!</p>
       <p>Adventure awaits!</p>
